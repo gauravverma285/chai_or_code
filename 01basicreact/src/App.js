16136f1de9e49c563d521084d70e81d1ad9767da
gauravverma285 <gaurav998290@gmail.com>
2023-10-09 17:11:18 +0530

    counter function is done

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+  // let counter = 15
+
+  const addValue = () => {
+    counter = counter + 1
+    if (counter < 21) {
+      setCounter(counter)
+    }
+    
+  }
+
+  const removeValue = () => {
+    counter = counter -1
+    if (counter >= 0) {
+      setCounter(counter)
+    }
+  }
+  return (
+    <> 
+    <h1>Hello Guarav {counter}</h1>
+    <button onClick={addValue}>Add value</button>
+
+    <button onClick={removeValue}>Remove value</button>
+    </>
+
+  );
+}
+
+export default App;
